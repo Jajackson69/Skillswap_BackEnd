60203d15de84375107db220986954a4408ec1734
Jajackson69 <jacksonrognardanoma@gmail.com>
2026-03-13 11:47:52 +0100

    Initial Spring Boot commit

---
+++ src/main/java/school/work/skillswap_b/SkillSwapBApplication.java
@@ -0,0 +1,13 @@
+package school.work.skillswap_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSwapBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSwapBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/work/skillswap_b/SkillSwapBApplicationTests.java
@@ -0,0 +1,13 @@
+package school.work.skillswap_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
