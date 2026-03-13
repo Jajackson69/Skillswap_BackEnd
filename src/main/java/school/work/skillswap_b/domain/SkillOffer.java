d2ca85bbfc5086faf86c09e20bdf957c872d56cd
Jajackson69 <jacksonrognardanoma@gmail.com>
2026-03-13 12:12:37 +0100

    Created develop branch feature

--- src/main/java/school/work/skillswap_b/domain/SkillOffer.java
+++ src/main/java/school/work/skillswap_b/domain/SkillOffer.java
@@ -1,5 +1,5 @@
 package school.work.skillswap_b.domain;
 
 public class SkillOffer {
-    //probably name & other stuff will go here, skillname, skillyearofexperience etc...
+    //probably name & other stuff will go here, skillname, skillyearofexperience etc....
 }
